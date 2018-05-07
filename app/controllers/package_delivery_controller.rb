a44909e5b0d930da0a116e40b192a1a7c816f18c
Bukalapak <rizkypascal@gmail.com>
2018-05-07 19:47:13 +0700

    add bot name in chat content

--- app/controllers/package_delivery_controller.rb
+++ app/controllers/package_delivery_controller.rb
@@ -10,11 +10,12 @@ class PackageDeliveryController < ApplicationController
     Rails.logger.info(tags: 'test-package', message: params.to_json)
     username = user[params[:full_name]]
     Telegram::Bot::Client.run(ENV['BOT_TOKEN']) do |bot|
-      bot.api.send_message(chat_id: ENV['BOT_GROUP'], text: "Halo Pengiriman untuk #{username} sudah dikirimkan ya, silakan cek detailnya di bot")
+      bot.api.send_message(chat_id: ENV['BOT_GROUP'], text: "Halo Pengiriman untuk #{username} sudah dikirimkan ya, silakan cek detailnya di @frontliner_bot")
     end
     render :json => params
   end
 
   def show
+
   end
 end
