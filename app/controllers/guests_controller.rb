1c7973f209d005420f522010dd38f8b5b0032037
antonk94 <antonkristanto94@gmail.com>
2018-05-08 16:53:05 +0700

    add chat_id

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,10 +1,10 @@
 class GuestsController < ApplicationController
   def notify
     user = {
-      'Anton Kristanto' => {id: '@antonkristanto', chat_id: 259897111},
-      # 'Bayu Surya Adi Saputra' => {'@bayusurya',
-      # 'Wasiro' => '@Wasiro',
-      'Rizky Paskalis Totong' => {id: '@rizkypascal', chat_id: 323049531}
+      'Anton Kristanto'        => {id: '@antonkristanto', chat_id: 259897111},
+      'Bayu Surya Adi Saputra' => {id: '@bayusurya',      chat_id: 269804643},
+      'Wasiro'                 => {id: '@wasiro',         chat_id: 76115512},
+      'Rizky Paskalis Totong'  => {id: '@rizkypascal',    chat_id: 323049531}
     }
     username = user[params[:full_name]][:id]
     chat = user[params[:full_name]][:chat_id]
--- app/controllers/package_delivery_controller.rb
+++ app/controllers/package_delivery_controller.rb
@@ -1,11 +1,12 @@
 class PackageDeliveryController < ApplicationController
   def create
     user = {
-      'Anton Kristanto' => {id: '@antonkristanto', chat_id: 259897111},
-      # 'Bayu Surya Adi Saputra' => {'@bayusurya',
-      # 'Wasiro' => '@Wasiro',
-      'Rizky Paskalis Totong' => {id: '@rizkypascal', chat_id: 323049531}
+      'Anton Kristanto'        => {id: '@antonkristanto', chat_id: 259897111},
+      'Bayu Surya Adi Saputra' => {id: '@bayusurya',      chat_id: 269804643},
+      'Wasiro'                 => {id: '@wasiro',         chat_id: 76115512},
+      'Rizky Paskalis Totong'  => {id: '@rizkypascal',    chat_id: 323049531}
     }
+
     username = user[params[:full_name]][:id]
     chat = user[params[:full_name]][:chat_id]
     Telegram::Bot::Client.run(ENV['BOT_TOKEN']) do |bot|
